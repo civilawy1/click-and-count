71f93ce185e6a1cde0c767d25782b532c708924c
PHClaus <phhpro@gmail.com>
2016-08-18 18:19:57 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,12 @@
 <?php
+//** Click And Count Config
+
+//** set data file
 $cac_dat = './data.txt';
+
+//** no need to edit below
 $cac_get = file_get_contents($cac_dat);
 $cac_ln  = explode("\n", $cac_get);
 $cac_ref = null;
 $cac_new = '';
-$cac_ver = '20160222';
+$cac_ver = '20160818';
