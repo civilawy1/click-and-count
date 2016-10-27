b6efa8a38120cd970a80fcd5c27a363d05acd238
PHClaus <phhpro@gmail.com>
2016-10-27 12:53:04 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,12 +1,17 @@
 <?php
 //** Click And Count Config
 
-//** set document root, script folder, data file, admin page and key
+/* document root
+ * script folder
+ * data file
+ * admin page
+ * admin key
+ */
 $cac_pub = '/home/www/public_html';
 $cac_dir = '/cac/';
-$cac_dat = 'data.txt';
-$cac_adi = 'admin.php';
-$cac_key = '1611';
+$cac_dat = './data.txt';
+$cac_adi = './admin.php';
+$cac_key = 'admin';
 
 
 //** no need to edit below
@@ -14,4 +19,4 @@ $cac_get = file_get_contents($cac_dat);
 $cac_ln  = explode("\n", $cac_get);
 $cac_ref = null;
 $cac_new = '';
-$cac_ver = '20161026';
+$cac_ver = '20161027';
